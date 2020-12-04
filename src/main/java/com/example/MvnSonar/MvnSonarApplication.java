ef410c2ce95606126c506c95e6696d85e464c896
Tanmay Shakya <tanmayshakya98@gmail.com>
2020-12-04 12:12:46 +0530

    Initial commit

---
+++ src/main/java/com/example/MvnSonar/MvnSonarApplication.java
@@ -0,0 +1,13 @@
+package com.example.MvnSonar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvnSonarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvnSonarApplication.class, args);
+	}
+
+}
